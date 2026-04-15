29cc9098c23ffe6a5f3d78a50826574068c2fc5b
stetteh-T <stetteh@terpmail.umd.edu>
2026-04-15 13:41:10 -0400

    Update nbaanalyzer.py
    In this commit, I added most of the functions that we think we are going to need, like rank_players function, and compare_players function. At some point we may need to add more.

--- nbaanalyzer.py
+++ nbaanalyzer.py
@@ -1,8 +1,29 @@
 class Player:
   pass
+  
+  def parsing_data(Newdata):
+    pass
+  
+  def group_players_by_team(players):
+    pass
 
+  def rank_players(players, stat_name):
+    pass
 
-def parsing_data(Newdata):
-  pass
+  def get_top_five(players, stat_name):
+    pass
 
+  def calc_team_avg(players, stat_name):
+
+  def compare_players(player1, player2):
+    pass
+
+  def get_team_choice():
+    pass
+
+  def get_stat_choice():
+    pass
+
+  display_results(team_name, top_players,average, stat_name):
+    pass
 
