3fe947b9ec4085909a31f4844f614a2874240fc0
stetteh-T <stetteh@terpmail.umd.edu>
2026-04-15 13:49:38 -0400

    Update nbaanalyzer.py
    Adding in the import function for the data.csv file, as well as cleaned up some mistakes I had.

--- nbaanalyzer.py
+++ nbaanalyzer.py
@@ -1,29 +1,36 @@
+import csv
+def load_data(file_name):
+  pass
+def parsing_data(Newdata):
+    pass
+
 class Player:
   pass
   
-  def parsing_data(Newdata):
+  def __init__(self, name, team, rebounds,points, assists):
     pass
+
+def group_players_by_team(players):
+  pass
+
+def rank_players(players, stat_name):
+  pass
+
+def get_top_five(players, stat_name):
+  pass
+
+def calc_team_avg(players, stat_name):
+  pass
   
-  def group_players_by_team(players):
-    pass
+def compare_players(player1, player2):
+  pass
 
-  def rank_players(players, stat_name):
-    pass
+def get_team_choice():
+  pass
 
-  def get_top_five(players, stat_name):
-    pass
+def get_stat_choice():
+  pass
 
-  def calc_team_avg(players, stat_name):
-
-  def compare_players(player1, player2):
-    pass
-
-  def get_team_choice():
-    pass
-
-  def get_stat_choice():
-    pass
-
-  display_results(team_name, top_players,average, stat_name):
+ def display_results(team_name, top_players,average, stat_name):
     pass
 
